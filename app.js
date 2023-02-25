61065660021ba7ebee22335f2a9f9845183356f4
Travis Tornquist <travis.tornquist@gmail.com>
2023-02-25 09:20:35 -0500

    fixed variable
    changed 'letFilteredData' to ' let Filtered Data'

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    letfilteredData = tableData;
+    let filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
